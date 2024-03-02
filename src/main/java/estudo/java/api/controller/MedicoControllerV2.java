6a7a32a060488246f4cae7d382b8e0442c72453a
Thomaz Honorato <thomaz.honorato@gmail.com>
2024-03-02 03:01:46 -0300

    Iniciado documentação da api com Spring openApi

--- src/main/java/estudo/java/api/controller/MedicoControllerV2.java
+++ src/main/java/estudo/java/api/controller/MedicoControllerV2.java
@@ -1,11 +1,10 @@
 package estudo.java.api.controller;
 
 import estudo.java.api.domain.request.MedicoRequest;
-import estudo.java.api.domain.request.MedicoRequest;
-import estudo.java.api.domain.response.MedicoResponse;
 import estudo.java.api.domain.response.MedicoResponse;
 import estudo.java.api.domain.validators.GrupoValidacao;
 import estudo.java.api.service.MedicoServiceV2;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,12 +22,18 @@ public class MedicoControllerV2 {
     @Autowired
     private MedicoServiceV2 medicoService;
 
+    @Operation(
+            summary = "Obtém uma lista de médicos cadastrados",
+            description = "Endpoint para listar todos os médicos cadastrados")
     @GetMapping
-    public ResponseEntity<List<MedicoResponse>> getAllMedicos(){
+    public ResponseEntity<List<MedicoResponse>> getAllMedicos() {
         List<MedicoResponse> medicos = medicoService.getAllMedicos();
         return ResponseEntity.ok(medicos);
     }
 
+    @Operation(
+            summary = "Obtém um médico pelo ID",
+            description = "Endpoint para buscar um médico cadastrado pelo seu ID")
     @GetMapping("/{id}")
     public ResponseEntity<MedicoResponse> getMedicoResponseById(@PathVariable UUID id) {
         MedicoResponse medicoResponse = medicoService.getMedicoById(id);
@@ -39,16 +44,25 @@ public class MedicoControllerV2 {
         }
     }
 
+    @Operation(
+            summary = "Cadastra um médico",
+            description = "Endpoint para cadastrar um médico")
     @PostMapping
     public ResponseEntity<MedicoResponse> createMedico(@RequestBody @Valid MedicoRequest medicoRequest) {
         return ResponseEntity.ok(medicoService.createMedico(medicoRequest));
     }
 
+    @Operation(
+            summary = "Altera um médico",
+            description = "Endpoint para alterar um médico cadastrado")
     @PutMapping("/{id}")
     public ResponseEntity<MedicoResponse> updateMedico(@PathVariable UUID id, @RequestBody @Validated(GrupoValidacao.Atualizar.class) MedicoRequest medicoRequest) {
         return ResponseEntity.ok(medicoService.updateMedico(id, medicoRequest));
     }
-    
+
+    @Operation(
+            summary = "Deleta um médico cadastrado",
+            description = "Endpoint Deletar um médico cadastrado")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMedico(@PathVariable UUID id) {
         medicoService.deleteMedico(id);
--- src/main/java/estudo/java/api/controller/PacienteControllerV2.java
+++ src/main/java/estudo/java/api/controller/PacienteControllerV2.java
@@ -4,6 +4,9 @@ import estudo.java.api.domain.request.PacienteRequest;
 import estudo.java.api.domain.response.PacienteResponse;
 import estudo.java.api.domain.validators.GrupoValidacao;
 import estudo.java.api.service.PacienteServiceV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +23,23 @@ public class PacienteControllerV2 {
     @Autowired
     private PacienteServiceV2 pacienteService;
 
-
+    @Operation(
+            summary = "Obtém uma lista de pacientes cadastrados",
+            description = "Endpoint para listar pacientes cadastrados"
+    )
+    @ApiResponse(responseCode = "200", description = "Busca efetuada com sucessso")
+    @ApiResponse(responseCode = "400", description = "Erro ao processar busca")
+    @ApiResponse(responseCode = "404", description = "Cadastros não encontrados")
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     @GetMapping
     public ResponseEntity<List<PacienteResponse>> getAllPacientes() {
         List<PacienteResponse> pacientes = pacienteService.getAllPacientes();
         return ResponseEntity.ok(pacientes);
     }
 
+    @Operation(
+            summary = "Obtém um paciente pelo ID",
+            description = "Endpoint para buscar um paciente cadastrado pelo seu ID")
     @GetMapping("/{id}")
     public ResponseEntity<PacienteResponse> getPacienteResponseById(@PathVariable UUID id) {
         PacienteResponse pacienteResponse = pacienteService.getPacienteById(id);
@@ -37,16 +50,25 @@ public class PacienteControllerV2 {
         }
     }
 
+    @Operation(
+            summary = "Cadastra um paciente",
+            description = "Endpoint para cadastrar um paciente")
     @PostMapping
     public ResponseEntity<PacienteResponse> createPaciente(@RequestBody @Validated(GrupoValidacao.Inserir.class) PacienteRequest pacienteRequest) {
         return ResponseEntity.ok(pacienteService.createPaciente(pacienteRequest));
     }
 
+    @Operation(
+            summary = "Altera um paciente cadastrado",
+            description = "Endpoint para alterar um paciente cadastrado")
     @PutMapping("/{id}")
     public ResponseEntity<PacienteResponse> updatePaciente(@PathVariable UUID id, @RequestBody @Validated(GrupoValidacao.Atualizar.class) PacienteRequest pacienteRequest) {
         return ResponseEntity.ok(pacienteService.updatePaciente(id, pacienteRequest));
     }
 
+    @Operation(
+            summary = "Deleta um paciente cadastrado",
+            description = "Endpoint deletar um paciente cadastrado")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePaciente(@PathVariable UUID id) {
         pacienteService.deletePaciente(id);
